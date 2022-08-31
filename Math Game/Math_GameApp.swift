49ce74a1b06a5c227726c498d35c972ac338ee73
Anton Nagorny <nagorny.anton@gmail.com>
2022-08-31 17:58:20 +0300

    – Add Firebase package and firebase init configure

--- Math Game/Math_GameApp.swift
+++ Math Game/Math_GameApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Math_GameApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
